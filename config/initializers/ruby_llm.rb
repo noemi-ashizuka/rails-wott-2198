81830aa878cb43b647889c013a8e08f2abf277de
noemi ashizuka <noemi.calanducci@gmail.com>
2026-02-25 19:24:25 +0900

    setup ruby_llm with azure

--- config/initializers/ruby_llm.rb
+++ config/initializers/ruby_llm.rb
@@ -1,8 +1,8 @@
 RubyLLM.configure do |config|
-  config.openai_api_key = ENV["OPENAI_API_KEY"]
+  # config.openai_api_key = ENV["OPENAI_API_KEY"]
   # NOTE(Eschults): Uncomment next two lines to use the Github Azure OpenAI playground
-  # config.openai_api_key = ENV["GITHUB_TOKEN"]
-  # config.openai_api_base = "https://models.inference.ai.azure.com"
+  config.openai_api_key = ENV["GITHUB_TOKEN"]
+  config.openai_api_base = "https://models.inference.ai.azure.com"
   # Add keys ONLY for providers you intend to use
   # config.anthropic_api_key = ENV.fetch('ANTHROPIC_API_KEY', nil)
   # ... see Configuration guide for all options ...
